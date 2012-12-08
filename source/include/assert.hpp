642354c6d3f78a506ca41ffaa74317e293f5b947
loonquawl <lquawl@gmail.com>
2012-12-08 12:15:42 +0100

    doingdoing

--- source/include/assert.hpp
+++ source/include/assert.hpp
@@ -5,7 +5,7 @@
 #define ASSERT(condition)		\
 		if (!(condition))	\
 		{			\
-			kpanic((condition) && "FAILED ASSERT");	\
+			kpanic((condition) && "FAILED ASSERT" && __FILE__ && __LINE__);	\
 		}
 
 #endif
--- source/include/memory/allocators.hpp
+++ source/include/memory/allocators.hpp
@@ -7,45 +7,89 @@
 	class PhysMemAllocator
 	{
 		public:
-			virtual char* alloc(Paging::PageSize size) = 0;
-			virtual void free(char* ptr) = 0;
+			/*
+			 * @brief	Returns page of a given size.
+			 * @return 	Page address or NULL.
+			 */
+			virtual char*	pageAlloc(Paging::PageSize size) = 0;
+			/*
+			 * @brief	Frees page.
+			 */
+			virtual void	pageFree(char* address) = 0;
+
+			/*
+			 * @brief	Returns address of a contiguous memory region
+			 * 		(several pages one after another).
+			 * @return	Address or NULL.
+			 */
+			virtual char*	alloc(size_t size);
+			/*
+			 * @brief	Frees all allocated contiguous pages.
+			 */
+			virtual void	free(char* address);
 	};
 
-	class BuddyAlloc : public PhysMemAllocator
+	class BuddyAllocator : public PhysMemAllocator
 	{
 		protected:
-
 			class Buddy
 			{
-				protected:				
-					uint64_t	page_map[8];
-					Buddy*		next_buddy[8*64];
-					PageSize	buddy_type;
-					size_t		region_start;
+				protected:
+					/*
+					 * @brief	Bit lit -> page used
+					 */
+					uint64_t	m_pagemap[8];
+					uint64_t	m_disabledpages[8];
+					/*
+					 * @brief	When you alloc a contiguous memory
+					 * 		region consisting of several pages.
+					 */
+					uint64_t	m_freenextpage[8];
+					Buddy*		m_nextbuddy[8*64];
+					PageSize	m_buddytype;
+					char*		m_regionstart;
 
+					void	initializeBuddyMap();					
+					void	nextFreePage(uint8_t* map_qword_num=NULL, uint8_t& qword_bit_num=NULL);
+	
 				public:
-					Buddy(PageSize type=PAGE1GB, size_t region_start)
-					: buddy_type(type), region_start(region_start)
+					Buddy(PageSize type=PAGE1GB, char* region_start);
+
+					void	freeAllPages();
+					char*	pageAlloc(Paging::PageSize size);
+					void	pageFree(char* ptr);
+
+					/*
+					 * @brief Pages after address will be disabled.
+					 * TODO
+					 */
+					void setUpperBound(char* address);
+
+					bool isFull()
 					{
-						kmemset((char*)(page_map),0xFF,sizeof(page_map));
-						for (Buddy* ptr=next; ptr<sizeof(next)/sizeof(*next); *ptr++=NULL);
+						return !~m_pagemap;
 					}
-
-					char* alloc(Paging::PageSize size);
-					void free(char* ptr);
 			};
+	
 
-			char*	memory;
-			size_t	size;
+			char*	m_regionstart;
+			size_t	m_regionsize;
 
-			uint64_t gigabyte_pages[];
-			uint64_t 
+			Buddy* m_gigabuddies[];
+			size_t m_gigabuddiesnum;
 
 		public:
-			BuddyAlloc();
+			BuddyAlloc(char* region_start, size_t region_size)
+			: m_regionstart(region_start), m_regionsize(region_size)
+			{
+				m_gigabuddiesnum=region_size/GIGABYTE + (region_size%GIGABYTE!=0);
+				m_gigabuddies = new Buddy*[m_gigabuddiesnum];
+				for (Buddy* ptr=m_gigabuddies; ptr<m_gigabuddies+m_gigabuddiesnum; *ptr++=NULL);
+				// TODO set_upper_bound
+			}
 
-			char*	alloc(size_t size);
-			void	free(char* ptr);
+			char*	pageAlloc(Paging::PageSize size);
+			void	pagefree(char* ptr);
 	};
 
 #endif
